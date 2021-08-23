5811252ff57639413d5bbb5b4462b36747de53e6
danielletate <88792165+danielletate@users.noreply.github.com>
2021-08-23 14:47:21 -0600

    Created function that displays user's age in days  when "click me button" is activated

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
-// Challenge 1: Your age in days
+function ageInDays() {
+  let birthYear = prompt("What year were you born?");
+  let numberOfDays = (2020 - birthYear) * 365;
+  let h1 = document.createElement("h1");
+  let textAnswer = document.createTextNode(`You are ${numberOfDays} days old!`);
+  h1.appendChild(textAnswer);
+  h1.setAttribute("id", "ageInDays");
+  document.getElementById("flex-box-result").appendChild(h1);
+}
